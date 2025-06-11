6ce1967324938a2e068e6d519bb8e79fe42a78f5
michaelpeluso <mfpel29@gmail.com>
2025-06-11 01:12:07 -0400

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()], // Enable React support
-    base: "/peluso-photos/", // Set the base path for GitHub Pages deployment
+    base: "/", // Set the base path for GitHub Pages deployment
 });
